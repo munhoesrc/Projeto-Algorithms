dac30a68803376c796447a6222214334cc919a6d
Renan Casarin Munhoes <munhoesrc@gmail.com>
2023-05-30 16:17:19 -0300

    feat: Req 1 e Req 2 (sendo requisito 2 não está passando 100%)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,19 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # """Faça o código aqui."""
+    # raise NotImplementedError
+    if target_time is None:
+        return None
+
+    students_no_periodo = 0
+
+    for student in permanence_period:
+        if (
+            not isinstance(student, tuple)
+            or len(student) != 2
+            or not all(isinstance(tempo, int) for tempo in student)
+        ):
+            return None
+        if student[0] <= target_time <= student[1]:
+            students_no_periodo += 1
+
+    return students_no_periodo
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,17 @@
 # from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    # pass
+    message = "SSOOLL"
+
+    assert encrypt_message(message, -1) == "LLOOSS"  # esseOK
+    assert encrypt_message(message, 1) == "S_LLOOS"
+    assert encrypt_message(message, 2) == "LLOO_SS"
+    assert encrypt_message(message, 3) == "OSS_LLO"  # esseOK
+    assert encrypt_message(message, 4) == "LL_OOSS"  # esseOK
+
+    with pytest.raises(TypeError):
+        encrypt_message(message, "")
